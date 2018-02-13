c00945d0eb5def95b7f8af462d7c44a9a89ab6a1
Timothy <timh1203@gmail.com>
2018-02-13 15:32:08 -0500

    Add core files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Tic-Tac-Toe Game</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
