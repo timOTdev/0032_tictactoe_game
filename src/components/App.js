90909297c9357d576abeecafcfd42d834b4855bf
Timothy <timh1203@gmail.com>
2018-02-17 13:12:50 -0500

    Refactor win conditions

--- src/components/App.js
+++ src/components/App.js
@@ -46,112 +46,32 @@ class App extends Component {
         board = ["","","","","","","","",""]
         this.setState({ board })
       }, 200)
-    } else if (updatedBoard[0] === "x" && updatedBoard[1] === "x" && updatedBoard[2] === "x") {
+    } else if (
+              (updatedBoard[0] === "x" && updatedBoard[1] === "x" && updatedBoard[2] === "x") ||
+              (updatedBoard[3] === "x" && updatedBoard[4] === "x" && updatedBoard[5] === "x") ||
+              (updatedBoard[6] === "x" && updatedBoard[7] === "x" && updatedBoard[8] === "x") ||
+              (updatedBoard[0] === "x" && updatedBoard[3] === "x" && updatedBoard[6] === "x") ||
+              (updatedBoard[1] === "x" && updatedBoard[4] === "x" && updatedBoard[7] === "x") ||
+              (updatedBoard[2] === "x" && updatedBoard[5] === "x" && updatedBoard[8] === "x") ||
+              (updatedBoard[0] === "x" && updatedBoard[4] === "x" && updatedBoard[8] === "x") ||
+              (updatedBoard[2] === "x" && updatedBoard[4] === "x" && updatedBoard[6] === "x") 
+              ) {
       let board = [...this.state.board]
       setTimeout(() => {
         alert("Player X wins!")
         board = ["","","","","","","","",""]
         this.setState({ board })
       }, 200)
-    } else if (updatedBoard[3] === "x" && updatedBoard[4] === "x" && updatedBoard[5] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[6] === "x" && updatedBoard[7] === "x" && updatedBoard[8] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[0] === "x" && updatedBoard[3] === "x" && updatedBoard[6] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[1] === "x" && updatedBoard[4] === "x" && updatedBoard[7] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[2] === "x" && updatedBoard[5] === "x" && updatedBoard[8] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[0] === "x" && updatedBoard[4] === "x" && updatedBoard[8] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[2] === "x" && updatedBoard[4] === "x" && updatedBoard[6] === "x") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        alert("Player X wins!")
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-      }, 200)
-    } else if (updatedBoard[0] === "o" && updatedBoard[1] === "o" && updatedBoard[2] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[3] === "o" && updatedBoard[4] === "o" && updatedBoard[5] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[6] === "o" && updatedBoard[7] === "o" && updatedBoard[8] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[0] === "o" && updatedBoard[3] === "o" && updatedBoard[6] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[1] === "o" && updatedBoard[4] === "o" && updatedBoard[7] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[2] === "o" && updatedBoard[5] === "o" && updatedBoard[8] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[0] === "o" && updatedBoard[4] === "o" && updatedBoard[8] === "o") {
-      let board = [...this.state.board]
-      setTimeout(() => {
-        board = ["","","","","","","","",""]
-        this.setState({ board })
-        alert("Player O wins!")
-      }, 200)
-    } else if (updatedBoard[2] === "o" && updatedBoard[4] === "o" && updatedBoard[6] === "o") {
+    } else if (
+              (updatedBoard[0] === "o" && updatedBoard[1] === "o" && updatedBoard[2] === "o") ||
+              (updatedBoard[3] === "o" && updatedBoard[4] === "o" && updatedBoard[5] === "o") ||
+              (updatedBoard[6] === "o" && updatedBoard[7] === "o" && updatedBoard[8] === "o") ||
+              (updatedBoard[0] === "o" && updatedBoard[3] === "o" && updatedBoard[6] === "o") ||
+              (updatedBoard[1] === "o" && updatedBoard[4] === "o" && updatedBoard[7] === "o") ||
+              (updatedBoard[2] === "o" && updatedBoard[5] === "o" && updatedBoard[8] === "o") ||
+              (updatedBoard[0] === "o" && updatedBoard[4] === "o" && updatedBoard[8] === "o") ||
+              (updatedBoard[2] === "o" && updatedBoard[4] === "o" && updatedBoard[6] === "o") 
+              ) {
       let board = [...this.state.board]
       setTimeout(() => {
         board = ["","","","","","","","",""]
