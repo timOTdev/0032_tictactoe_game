a50a968d32445cb66c41b3ca1c2562ba3290c67a
Timothy <timh1203@gmail.com>
2018-03-04 14:01:35 -0500

    Add readme and stylings

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div className="Intro">
+    <div className="Intro" id="Intro">
       <h2>Let's play a game!</h2>
     </div>
   )
